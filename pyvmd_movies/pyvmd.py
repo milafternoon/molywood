1579350278fa7c8e4dc6f303616030d4fe1dc95d
milafternoon <milafternoon@wp.pl>
2019-04-25 15:29:48 +0200

    example user-defined script + input parsing

--- pyvmd_movies/pyvmd.py
+++ pyvmd_movies/pyvmd.py
@@ -6,6 +6,7 @@ class Script:
     """
     def __init__(self):
         self.subscripts = []
+        self.directives = {}
     
     def render(self, draft=False, framerate=20, keepframes=False):
         """
@@ -26,6 +27,7 @@ class Script:
         :return: None
         """
         for subscript in self.subscripts:
+            print('Subscript {}: \n\n'.format(subscript.name))
             subscript.show_script()
 
     def from_file(self, filename):
@@ -34,8 +36,59 @@ class Script:
         :param filename: str, name of the file
         :return: None
         """
-        pass
-
+        script = [line.strip() for line in open(filename, 'r')]
+        current_sub = '_default'
+        subscripts = {current_sub: []}
+        multiline = None
+        master_setup = []
+        for line in script:
+            if line.startswith('#'):  # beginning of a subscript
+                current_sub = line.strip('#').strip()
+                subscripts[current_sub] = []
+            elif line.startswith('$'):  # global directives
+                master_setup.append(line.strip('$').strip())
+            elif line:  # regular content of subscript
+                if line.startswith('{'):  # with a possibility of multi-actions wrapped into curly brackets
+                    multiline = line
+                elif multiline and '}' not in line:
+                    multiline = multiline + line
+                elif multiline and '}' in line:
+                    subscripts[current_sub].append(multiline + line)
+                    multiline = None
+                else:
+                    subscripts[current_sub].append(line)
+        self.directives = self.parse_directives(master_setup)
+        self.subscripts = self.parse_subscripts(subscripts)
+        
+    @staticmethod
+    def parse_directives(directives):
+        dirs = {}
+        for directive in directives:
+            entries = directive.split()
+            dirs[entries[0]] = {}
+            for entry in entries[1:]:
+                key, value = entry.split('=')
+                dirs[entries[0]][key] = value
+        return dirs
+    
+    def parse_subscripts(self, subscripts):
+        objects = []
+        struct, traj = None, None
+        for sub in subscripts.keys():
+            if sub in self.directives.keys():
+                try:
+                    struct = self.directives[sub]['structure']
+                except KeyError:
+                    pass
+                try:
+                    traj = self.directives[sub]['trajectory']
+                except KeyError:
+                    pass
+            objects.append(Subscript(sub, struct, traj))
+            for action in subscripts[sub]:
+                objects[-1].add_action(action)
+        return objects
+        
 
 class Subscript:
     """
@@ -43,27 +96,20 @@ class Subscript:
     molecular system, and hence has to be initialized
     with a valid input file
     """
-    def __init__(self, molecule_file, trajs=None):
+    def __init__(self, name, molecule_file=None, traj=None):
+        self.name = name
         self.system = molecule_file
-        self.trajs = []
-        if trajs:
-            if isinstance(trajs, str):
-                self.trajs = [trajs]
-            else:
-                self.trajs = trajs
+        self.traj = traj
         self.actions = []
     
-    def add_trajs(self, trajs):
+    def add_traj(self, traj):
         """
-        Allows to add trajectories to VMD after the
+        Allows to add a trajectory to VMD after the
         object was initialized
-        :param trajs: str or list of str, trajectory filename(s)
+        :param traj: str, trajectory filename
         :return: None
         """
-        if isinstance(trajs, str):
-            self.trajs.append(trajs)
-        else:
-            self.trajs.extend(trajs)
+        self.traj = traj
     
     def add_action(self, description):
         """
@@ -71,10 +117,10 @@ class Subscript:
         :param description: str, description of the action
         :return: None
         """
-        if 'and' not in description:
+        if not description.startswith('{'):
             self.actions.append(Action(description))
         else:
-            self.actions.append(SimultaneousAction(description.split(' and ')))
+            self.actions.append(SimultaneousAction(description))
 
     def show_script(self):
         """
@@ -126,5 +172,5 @@ class SimultaneousAction:
     that take place simultaneously (e.g. zoom
     and rotation)
     """
-    def __init__(self, descriptions):
-        self.actions = [Action(description) for description in descriptions]
+    def __init__(self, description):
+        self.description = description
